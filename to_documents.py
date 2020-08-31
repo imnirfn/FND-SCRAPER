e76840e23975c2f2975a38e25f3c040e4556cb6f
imnirfn <s3ns3xy@yahoo.com>
2020-09-01 00:22:54 +0800

    data return

--- to_documents.py
+++ to_documents.py
@@ -1,14 +1,30 @@
 import newspaper as ns
 import boto3
+import requests
+import json
 
 def handler(event, context):
 
     ACCESS_KEY_ID = 'AKIAUPGXYIMMXYPTFWMH'
     ACCESS_SECRET_KEY = 'OqnPxNLRypT+6zj8GHCKabiQbh3oKIJYaGL6wN97'
     BUCKET_NAME = 'tinggitecc'
-    FILE_NAME = "docs_text_sample.txt"
+    FILE_NAME = "news.txt"
 
-    data = func()
+    # json.loads(json.dumps(event))
+    url = json.loads(json.dumps(event))
+    parse = ns.build(url["data"], language='en')
+    
+    article = parse.articles[0]
+    article.download()
+    article.parse()
+
+    print(article.text)
+    data = article.text
+
+    # return response
+    x = {
+        "data": article.text
+    }
 
     # S3 Connect
     s3 = boto3.resource(
@@ -17,19 +33,35 @@ def handler(event, context):
         aws_secret_access_key=ACCESS_SECRET_KEY
     )
 
-    # Uploaded File
+    # Uploade File to S3
     s3.Bucket(BUCKET_NAME).put_object(Key=FILE_NAME, Body=data)
 
-def func():
-    url = ns.build('https://newspaper.readthedocs.io/en/latest/', language='en')
+    return x["data"]
+    
+def to_docs():
+    # json.loads(json.dumps(event))
+    event = { 
+        "data": "https://www.malaymail.com/news/malaysia/2020/08/31/malaysia-reports-second-covid-19-death-in-two-days-six-new-cases/1898831", 
+        "date": "April20"
+    }
 
-    for category in url.category_urls():
-        print(category)
-
-    article = url.articles[0]
+    url = json.loads(json.dumps(event))
+    print(url["data"])
+    parse = ns.build(url["data"], language='zh')
+    
+    article = parse.articles[0]
     article.download()
     article.parse()
 
     print(article.text)
-    return article.text
 
+    x = {
+        "article_text": article.text
+    }
+
+    data = json.loads(json.dumps(x))
+    print(data)
+    return data
+
+
+to_docs()
